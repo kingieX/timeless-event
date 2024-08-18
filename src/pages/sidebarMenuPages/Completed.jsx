362c9f31be827b01a82a89600140bb952b729dbb
kingieX <chimakingsley216@gmail.com>
2024-08-18 22:45:45 +0100

    update: COmpleted page in progress

--- src/pages/sidebarMenuPages/Completed.jsx
+++ src/pages/sidebarMenuPages/Completed.jsx
@@ -1,11 +1,30 @@
 // import React from 'react'
-
-import UnderConstruction from '../../components/UnderConstruction';
+import { useState } from 'react';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
 const Completed = () => {
+  const [showCompletedTasks, setShowCompletedTasks] = useState(true);
+
+  const toggleTasks = () => setShowCompletedTasks(!showCompletedTasks);
+
   return (
-    <div>
-      <UnderConstruction />
+    <div className="w-full lg:p-10 py-6 lg:px-8 px-4">
+      {/* Header Section */}
+      <div className="flex">
+        <div
+          className="flex flex-row justify-center items-center lg:gap-2 cursor-pointer"
+          onClick={toggleTasks}
+        >
+          <h1 className="lg:text-2xl text-xl font-bold flex items-center">
+            Activity: All projects
+          </h1>
+          {showCompletedTasks ? (
+            <FaChevronUp className="mr-2 text-slate-600 w-3" />
+          ) : (
+            <FaChevronDown className="mr-2 text-slate-600 w-3" />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
