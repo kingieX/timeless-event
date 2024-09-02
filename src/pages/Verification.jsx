49adce7493133d4e405bfabbcc1de28db202adc0
kingieX <chimakingsley216@gmail.com>
2024-09-02 05:26:17 +0100

    Update: adjustment on mobile view for the verification page

--- src/pages/Verification.jsx
+++ src/pages/Verification.jsx
@@ -87,7 +87,7 @@ const VerificationPage = () => {
         </Link>
       </div>
 
-      <div className="flex justify-center items-center lg:min-h-screen">
+      <div className="flex justify-center items-center min-h-screen">
         <div className="bg-white py-8 lg:px-12 px-8 border m-8 border-gray shadow-lg rounded-md w-full max-w-md md:max-w-lg lg:max-w-2xl">
           <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center lg:mb-6 mb-2">
             {verificationMethod === 'email'
