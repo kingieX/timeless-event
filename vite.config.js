89e572b59981c2e34d277e967ffdbfdca64d2d9d
nnachi kingsley <chimakingsley216@gmail.com>
2024-10-25 22:08:30 +0100

    Update: bug fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
   build: {
     chunkSizeWarningLimit: 2000,
     // Ensures only the necessary code is included in the build
